5a4c74270501801291dc18159bb8c57704f9694a
cooler <maxkux2016@gmail.com>
2025-11-07 18:56:40 +0300

    Removed hardcode

--- app/src/main/java/com/example/vkeducationkotlindz1/MainActivity.kt
+++ app/src/main/java/com/example/vkeducationkotlindz1/MainActivity.kt
@@ -4,46 +4,35 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.*
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.integerResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.min
 import androidx.compose.ui.unit.sp
-import com.example.vkeducationkotlindz1.ui.theme.VkEducationKotlinDz1Theme
-import kotlinx.serialization.builtins.serializer
-import java.util.Collections.list
+import androidx.compose.ui.unit.times
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,56 +46,69 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    var count by rememberSaveable { mutableIntStateOf(5) }
+    val initialBoxesCount: Int = integerResource(R.integer.initial_cells_count)
+    var boxesCount by rememberSaveable { mutableIntStateOf(value = initialBoxesCount) }
+    val listSidePadding: Dp = dimensionResource(R.dimen.list_border_padding)
+    val listInternalPadding: Dp = dimensionResource(R.dimen.inter_boxes_padding)
+    val actionButtonPadding: Dp = dimensionResource(R.dimen.fab_padding)
+    val actionButtonSize: Dp = dimensionResource(R.dimen.fab_size)
     val screenPosition = LocalConfiguration.current.orientation
-    val columnsCount = if (screenPosition == Configuration.ORIENTATION_LANDSCAPE) {
-        4
-    } else {
-        3
-    }
+    val columnsCount = if (screenPosition == Configuration.ORIENTATION_LANDSCAPE)
+        integerResource(R.integer.horizontal_orientation_columns_count)
+    else
+        integerResource(R.integer.vertical_orientation_columns_count)
+
+
+
     Box(modifier = Modifier.fillMaxSize()) {
         LazyVerticalGrid(
             columns = GridCells.Fixed(count = columnsCount),
             modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(top = 5.dp, start = 5.dp, end = 5.dp, bottom = 100.dp),
-            verticalArrangement = Arrangement.spacedBy(space = 10.dp),
-            horizontalArrangement = Arrangement.spacedBy(space = 10.dp),
+            contentPadding = PaddingValues(
+                top = listSidePadding,
+                start = listSidePadding,
+                end = listSidePadding,
+                bottom = 2 * actionButtonPadding + actionButtonSize,
+            ),
+            verticalArrangement = Arrangement.spacedBy(space = listInternalPadding),
+            horizontalArrangement = Arrangement.spacedBy(space = listInternalPadding),
         ) {
 
             items(
-                count = count,
+                count = boxesCount,
             ) {
                 Box(
                     contentAlignment = Alignment.Center,
                     modifier = Modifier
                         .aspectRatio(1f)
-//                        .padding(5.dp)
                         .background(
-                            color = if (it % 2 == 0) {
-                                Color.Red
-                            } else {
-                                Color.Blue
-                            }
-                        ),
+                            color = if (it % 2 == 0)
+                                colorResource(R.color.odd_color)
+                            else
+                                colorResource(R.color.even_color)
+
+                        )
+                        ,
 
                     ) {
                     Text(
                         text = "$it",
-                        fontSize = 30.sp,
-                        color = Color.Green,
+                        fontSize = integerResource(R.integer.inbox_text_size).sp,
+                        color = colorResource(R.color.inbox_text_color),
                     )
                 }
             }
         }
         FloatingActionButton(
-            onClick = { count++ },
+            onClick = { boxesCount++ },
             modifier = Modifier
                 .align(alignment = Alignment.BottomEnd)
-                .padding(30.dp)
+                .padding(all = actionButtonPadding)
+                .size(size = actionButtonSize)
         ) {
             Text(
                 text = "+",
-                fontSize = 50.sp,
+                fontSize = integerResource(R.integer.fab_text_size).sp,
             )
         }
     }
